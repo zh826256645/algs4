175b12fa64ca18b5c83a0cee2a75c453fe914228
ZhongHao <zh826256645@gmail.com>
2018-06-11 22:13:56 +0800

    深度优先搜索

--- src/com/four/one/TestSearch.java
+++ src/com/four/one/TestSearch.java
@@ -6,7 +6,8 @@ public class TestSearch {
     public static void main(String[] args) {
         Graph G = new Graph(new In(args[0]));
         int s = Integer.parseInt(args[1]);
-        Search search = new Search(G, s);
+//        Search search = new Search(G, s);
+        DepthFirstSearch search = new DepthFirstSearch(G, s);
 
         for (int v = 0; v < G.V(); v++) {
             if (search.marked(v)) {
@@ -16,7 +17,7 @@ public class TestSearch {
         System.out.println();
 
         if (search.count() != G.V()) {
-            System.out.println("NOT ");
+            System.out.print("NOT ");
         }
         System.out.println("connected");
     }
